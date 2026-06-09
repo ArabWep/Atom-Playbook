94811098f85879fbf8c4e2294eb89146f6dc4ef5
ArabWep <osgamerbgghjg@gmail.com>
2026-06-09 20:05:22 +0300

    updating post install folder

--- src/Executables/Copy-PostInstall.ps1
+++ src/Executables/Copy-PostInstall.ps1
@@ -1,4 +1,5 @@
-$src = Join-Path $PSScriptRoot "PostInstall"
+$playbookRoot = Split-Path $PSScriptRoot -Parent
+$src = Join-Path $playbookRoot "PostInstall"
 $dst = Join-Path $env:USERPROFILE "Desktop\PostInstall"
 if (Test-Path $src) {
     Copy-Item -Path $src -Destination $dst -Recurse -Force
